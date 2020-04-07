a558553c06dedf718f0dce1c49651ee61075a66c
Omer S Lewis <contact@oslewiscom>
2020-04-07 18:37:15 +0100

    added plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
 const webpack = require('webpack')
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
 
 module.exports = {
     entry: './src/client/index.js',
@@ -9,7 +11,18 @@ module.exports = {
             test: '/\.js$/',
             exclude: /node_modules/,
             loader: "babel-loader"
-                }
+                },
+                {
+                    test: /\.json$/,
+                    loader: 'json-loader'
+                  }
         ]
-}
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/client/views/index.html",
+            filename: "./index.html",
+        })
+    ]
+
 }
